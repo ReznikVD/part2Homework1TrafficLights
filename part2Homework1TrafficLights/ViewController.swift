b6b61de58ac107c9a291a9a3ca4a377aeddb27b1
ReznikVD <vladek_1999@mail.ru>
2021-12-14 14:32:21 +0100

    Initial Commit

---
+++ part2Homework1TrafficLights/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  part2Homework1TrafficLights
+//
+//  Created by user207855 on 12/14/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
